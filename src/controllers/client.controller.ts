948199482fa850c1349eae0651e98af100cfed6f
RnCeintec <ceintec.rn@gmail.com>
2022-07-22 17:45:34 -0500

    Cambios en Tiendas y Configuracion de Perfil

--- src/controllers/client.controller.ts
+++ src/controllers/client.controller.ts
@@ -1,150 +1,171 @@
-import { Sales } from 'core/entities';
-import {Response,Request} from 'express'
-import { client } from 'security';
-import {getRepository, ObjectLiteral, FindConditions,In,Like,Raw} from 'typeorm'
-import {Client} from '../core/entities/client'
-import {createClientInteractor,updateClientInteractor,deleteClientInteractor} from '../core/interactor/client';
-import { encrypt } from '../utils';
-import { Hateoas } from '../utils';
+import { Sales } from "core/entities";
+import { Response, Request } from "express";
+import { client } from "security";
+import {
+  getRepository,
+  ObjectLiteral,
+  FindConditions,
+  In,
+  Like,
+  Raw,
+} from "typeorm";
+import { Client } from "../core/entities/client";
+import {
+  createClientInteractor,
+  updateClientInteractor,
+  deleteClientInteractor,
+} from "../core/interactor/client";
+import { encrypt } from "../utils";
+import { Hateoas } from "../utils"; //
 
+export const createClient = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  try {
+    const {
+      rz_social,
+      direccion,
+      documento,
+      telefono,
+      observacion,
+      tipo_documento,
+    } = req.body;
+    const cliente = new Client();
 
-export const createClient = async (req:Request,res:Response): Promise<Response> =>{
-    try{
-        const {     rz_social, direccion,documento,telefono,observacion,tipo_documento } = req.body
-        const cliente = new Client()
+    cliente.rz_social = rz_social;
+    cliente.direccion = direccion;
+    cliente.tipo_documento = tipo_documento;
+    cliente.documento = documento;
+    cliente.telefono = telefono;
+    cliente.observacion = observacion;
 
-        cliente.rz_social = rz_social 
-        cliente.direccion = direccion 
-        cliente.tipo_documento = tipo_documento 
-        cliente.documento = documento 
-        cliente.telefono = telefono
-        cliente.observacion = observacion 
-
-
-
-        const result = await createClientInteractor(cliente)
-        return res.json({result:result})
-
-    }catch(error:any){
-        throw res.status(500).json({message: error.message ?? error})
-
-    }
-
-    
-
-}
-
-export const updateClient= async (req:Request,res:Response): Promise<Response> =>{
-    try{
-        const { rz_social, direccion,documento,telefono,observacion,tipo_documento } = req.body
-        const cliente = await getRepository(Client).findOne(req.params.id)
-        if(!cliente){
-          return  res.status(404).json({message:"Dede enviar id de cliente"})
-        }
-
-        cliente.rz_social = rz_social ??  cliente.rz_social
-        cliente.direccion = direccion ??  cliente.direccion
-        cliente.tipo_documento = tipo_documento ?? cliente.tipo_documento
-        cliente.documento = documento ??  cliente.documento
-        cliente.telefono = telefono ??  cliente.telefono
-        cliente.observacion = observacion ??  cliente.observacion
-        const result = await updateClientInteractor(cliente)
-        return res.json({result:result})
-
-    }catch(error:any){
-        throw res.status(500).json({message: error.message ?? error})
-
-    }
-}
-
-export const deleteClient = async (req:Request,res:Response): Promise<Response> =>{
-    try{
-        const clientById= await getRepository(Client).findOne(req.params.id)
-        if(!clientById){
-            return res.status(404).json({message:"No existe el cliente"})
-        }
-        const result = await deleteClientInteractor(clientById)
-        return res.json({result:result})
-
-    }catch(error:any){
-        throw res.status(500).json({message: error.message ?? error})
-
-    }
-}
-
-export const listClient = async (req:Request,res:Response): Promise<Response> =>{
-    try {
-        const {limit, offset, search } = req.query;
-        const hateoas = new Hateoas({
-            limit: limit ? `${limit}` : undefined,
-            offset: offset
-              // ? search && search !== ''
-              //   ? undefined
-                ? `${offset}`
-              : undefined,
-          });
-        const take = hateoas.take;
-        const skip = hateoas.skip;
-        let where:
-        | string
-        | ObjectLiteral
-        | FindConditions<Client>
-        | FindConditions<Client>[]
-        | undefined = { isActive: true };
-        
-        const  [result, count] = await getRepository(Client).findAndCount({
-            take,
-            skip: skip * take,
-            where : [
-                {
-                    rz_social: Like(`%${search}%`),
-                    ...where
-                  },
-                  {
-                    documento: Like(`%${search}%`),
-                    ...where
-                  },
-                  {
-                    telefono: Like(`%${search}%`),
-                    ...where
-                  },
-                    {
-                        id: Like(`%${search}%`),
-                        ...where,
-                  }
-         
-          
-          ],
-                order: {fecha_actualizacion:"DESC"}
-          });
-    
-          const [hateoasLink, pages] = hateoas.hateoas({ count });
-          return result
-            ? res.status(200).json({
-                result,
-                count,
-                link: hateoasLink,
-                pages: pages === 0 ? 1 : pages,
-              })
-            : res.status(404).json({ message: 'No existen clientes' });
-      }catch(error:any){
-        throw res.status(500).json({message: error.message ?? error})
-      }
-}
-
-
-export const  searchClient  = async (req:Request,res:Response): Promise<Response> =>{
-
-  try{
-      const cliente = await getRepository(Client).findOne(req.params.id)
-      if(!cliente){
-          return res.status(404).json({message:"No existe el producto"})
-      }
-      return res.status(200).json({result:cliente})
-  }catch(error:any){
-      throw res.status(500).json({message: error.message ?? error})
-
+    const result = await createClientInteractor(cliente);
+    return res.json({ result: result });
+  } catch (error: any) {
+    throw res.status(500).json({ message: error.message ?? error });
   }
+};
 
+export const updateClient = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  try {
+    const {
+      rz_social,
+      direccion,
+      documento,
+      telefono,
+      observacion,
+      tipo_documento,
+    } = req.body;
+    const cliente = await getRepository(Client).findOne(req.params.id);
+    if (!cliente) {
+      return res.status(404).json({ message: "Dede enviar id de cliente" });
+    }
 
-}
+    cliente.rz_social = rz_social ?? cliente.rz_social;
+    cliente.direccion = direccion ?? cliente.direccion;
+    cliente.tipo_documento = tipo_documento ?? cliente.tipo_documento;
+    cliente.documento = documento ?? cliente.documento;
+    cliente.telefono = telefono ?? cliente.telefono;
+    cliente.observacion = observacion ?? cliente.observacion;
+    const result = await updateClientInteractor(cliente);
+    return res.json({ result: result });
+  } catch (error: any) {
+    throw res.status(500).json({ message: error.message ?? error });
+  }
+};
+
+export const deleteClient = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  try {
+    const clientById = await getRepository(Client).findOne(req.params.id);
+    if (!clientById) {
+      return res.status(404).json({ message: "No existe el cliente" });
+    }
+    const result = await deleteClientInteractor(clientById);
+    return res.json({ result: result });
+  } catch (error: any) {
+    throw res.status(500).json({ message: error.message ?? error });
+  }
+};
+
+export const listClient = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  try {
+    const { limit, offset, search } = req.query;
+    const hateoas = new Hateoas({
+      limit: limit ? `${limit}` : undefined,
+      offset: offset
+        ? // ? search && search !== ''
+          //   ? undefined
+          `${offset}`
+        : undefined,
+    });
+    const take = hateoas.take;
+    const skip = hateoas.skip;
+    let where:
+      | string
+      | ObjectLiteral
+      | FindConditions<Client>
+      | FindConditions<Client>[]
+      | undefined = { isActive: true };
+
+    const [result, count] = await getRepository(Client).findAndCount({
+      take,
+      skip: skip * take,
+      where: [
+        {
+          rz_social: Like(`%${search}%`),
+          ...where,
+        },
+        {
+          documento: Like(`%${search}%`),
+          ...where,
+        },
+        {
+          telefono: Like(`%${search}%`),
+          ...where,
+        },
+        {
+          id: Like(`%${search}%`),
+          ...where,
+        },
+      ],
+      order: { fecha_actualizacion: "DESC" },
+    });
+
+    const [hateoasLink, pages] = hateoas.hateoas({ count });
+    return result
+      ? res.status(200).json({
+          result,
+          count,
+          link: hateoasLink,
+          pages: pages === 0 ? 1 : pages,
+        })
+      : res.status(404).json({ message: "No existen clientes" });
+  } catch (error: any) {
+    throw res.status(500).json({ message: error.message ?? error });
+  }
+};
+
+export const searchClient = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  try {
+    const cliente = await getRepository(Client).findOne(req.params.id);
+    if (!cliente) {
+      return res.status(404).json({ message: "No existe el producto" });
+    }
+    return res.status(200).json({ result: cliente });
+  } catch (error: any) {
+    throw res.status(500).json({ message: error.message ?? error });
+  }
+};
--- src/controllers/shop.controller.ts
+++ src/controllers/shop.controller.ts
@@ -1,5 +1,11 @@
-import { Shop } from "../core/entities";
+import { Shop } from "../core/entities/shop";
+import {
+  createShopInteractor,
+  updateShopInteractor,
+  deleteShopInteractor,
+} from "../core/interactor/shop";
 import { Response, Request } from "express";
+import { Hateoas } from "../utils";
 import {
   getRepository,
   ObjectLiteral,
@@ -9,7 +15,54 @@ import {
   Raw,
 } from "typeorm";
 
-export const listShop = async (
+export const createShop = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  try {
+    const {
+      nombre,
+      direccion,
+      telefono,
+      correo,
+      ciudad,
+      num_documento,
+      rz_social,
+    } = req.body;
+    const shop = new Shop();
+
+    shop.nombre = nombre;
+    shop.direccion = direccion;
+    shop.telefono = telefono;
+    shop.correo = correo;
+    shop.ciudad = ciudad;
+    shop.num_documento = num_documento;
+    shop.rz_social = rz_social;
+
+    const result = await createShopInteractor(shop);
+    return res.json({ result: result });
+  } catch (error: any) {
+    throw res.status(500).json({ message: error.message ?? error });
+  }
+};
+
+export const deleteShop = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  try {
+    const shopById = await getRepository(Shop).findOne(req.params.id);
+    if (!shopById) {
+      return res.status(404).json({ message: "No existe el cliente" });
+    }
+    const result = await deleteShopInteractor(shopById);
+    return res.json({ result: result });
+  } catch (error: any) {
+    throw res.status(500).json({ message: error.message ?? error });
+  }
+};
+
+/* export const listShop = async (
   req: Request,
   res: Response
 ): Promise<Response> => {
@@ -22,8 +75,70 @@ export const listShop = async (
   }
 
   return res.json({ result: shop });
+}; */
+//------------------------------------------------
+export const listShop = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  try {
+    const { limit, offset, search } = req.query;
+    const hateoas = new Hateoas({
+      limit: limit ? `${limit}` : undefined,
+      offset: offset
+        ? // ? search && search !== ''
+          //   ? undefined
+          `${offset}`
+        : undefined,
+    });
+    const take = hateoas.take;
+    const skip = hateoas.skip;
+    let where:
+      | string
+      | ObjectLiteral
+      | FindConditions<Shop>
+      | FindConditions<Shop>[]
+      | undefined = { isActive: true };
+
+    const [result, count] = await getRepository(Shop).findAndCount({
+      take,
+      skip: skip * take,
+      where: [
+        {
+          rz_social: Like(`%${search}%`),
+          ...where,
+        },
+        {
+          nombre: Like(`%${search}%`),
+          ...where,
+        },
+        /* {
+                  telefono: Like(`%${search}%`),
+                  ...where
+                }, */
+        {
+          id: Like(`%${search}%`),
+          ...where,
+        },
+      ],
+      order: { fecha_actualizacion: "DESC" },
+    });
+
+    const [hateoasLink, pages] = hateoas.hateoas({ count });
+    return result
+      ? res.status(200).json({
+          result,
+          count,
+          link: hateoasLink,
+          pages: pages === 0 ? 1 : pages,
+        })
+      : res.status(404).json({ message: "No existen Locales" });
+  } catch (error: any) {
+    throw res.status(500).json({ message: error.message ?? error });
+  }
 };
 
+//------------------------------------------------
 export const updateShop = async (
   req: Request,
   res: Response
@@ -34,10 +149,10 @@ export const updateShop = async (
     telefono,
     correo,
     num_documento,
-    eslogan,
+    //eslogan,
     ciudad,
     rz_social,
-    codDomicilioFiscal,
+    //codDomicilioFiscal,
   } = req.body;
 
   const shop = await getRepository(Shop).findOne(req.params.id);
@@ -50,10 +165,10 @@ export const updateShop = async (
   shop.telefono = telefono ?? shop.telefono;
   shop.correo = correo ?? shop.correo;
   shop.num_documento = num_documento ?? shop.num_documento;
-  shop.eslogan = eslogan ?? shop.eslogan;
+  //shop.eslogan = eslogan ?? shop.eslogan;
   shop.ciudad = ciudad ?? shop.ciudad;
   shop.rz_social = rz_social ?? shop.rz_social;
-  shop.codDomicilioFiscal = codDomicilioFiscal ?? shop.codDomicilioFiscal;
+  //shop.codDomicilioFiscal = codDomicilioFiscal ?? shop.codDomicilioFiscal;
 
   //    getRepository(Shop).merge(shop,shop)
 
@@ -66,7 +181,7 @@ export const searchShop = async (
   res: Response
 ): Promise<Response> => {
   try {
-    //PASAR LAS CATEGPRIAS RELACIONADAS
+    //PASAR LAS CATEGPRIAS RELACIONADAS*
     const local = await getRepository(Shop).findOne(req.params.id);
     if (!local) {
       return res.status(404).json({ message: "No existe el local" });
