13f22bfad49351906bb4e9d56810c056ee3c2598
RnCeintec <ceintec.rn@gmail.com>
2022-07-25 15:12:10 -0500

    Vendedores corregido

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,17 +1,17 @@
-import { Express } from 'express';
+import { Express } from "express";
 
-import userRoute from './user.router';
-import loginRoute from './auth.route';
-import productRoute from './product.route';
-import categoryRoute from './category.route';
-import clientRoute from './client.route';
-import salesRoute from './sales.route';
-import pymentTypesRoute from './pymentTypes.route';
-import shopRoute from './shop.route';
-import dashboardRoute from './dashboar.route';
+import userRoute from "./user.router";
+import loginRoute from "./auth.route";
+import productRoute from "./product.route";
+import categoryRoute from "./category.route";
+import clientRoute from "./client.route";
+import salesRoute from "./sales.route";
+import pymentTypesRoute from "./pymentTypes.route";
+import shopRoute from "./shop.route";
+import dashboardRoute from "./dashboar.route";
 
-import facturasRoute from './facturas.routes';
-import vendedorRoute from './vendedor.route';
+import facturasRoute from "./facturas.routes";
+import vendedorRoute from "./vendedor.route";
 
 export default ({ app, version }: { app: Express; version: string }) => {
   app.use(version, userRoute);
@@ -24,5 +24,4 @@ export default ({ app, version }: { app: Express; version: string }) => {
   app.use(version, shopRoute);
   app.use(version, dashboardRoute);
   app.use(version, vendedorRoute);
-
 };
